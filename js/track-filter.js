ba819bc4f6b523d9c8a7d022a149ac6803a21d9f
Nacho Agudo <nachoecha@gmail.com>
2020-08-22 10:33:25 +0200

    seleccion filtro rutas

--- js/track-filter.js
+++ js/track-filter.js
@@ -2,11 +2,10 @@
   $("#filterList .c-filters__item").on("click", function () {
     if ($(this).hasClass("c-filters__item--active")) {
       $(this).removeClass("c-filters__item--active")
-      $("#filterList button").removeClass("c-filters__item--deactive")
       $("#routeList a").show();
     } else {
-      $("#filterList button").removeClass("c-filters__item--active").addClass("c-filters__item--deactive");
-      $(this).addClass("c-filters__item--active").removeClass("c-filters__item--deactive");
+      $("#filterList button").removeClass("c-filters__item--active");
+      $(this).addClass("c-filters__item--active");
 
       var selectedTrack = $(this).data("route-class");
       var showTrack = "." + selectedTrack;
